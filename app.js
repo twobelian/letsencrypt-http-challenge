e10ab298db349bbbb67aae3a4510365ba7b973fb
Lian <lianlin@iq-t.com>
2020-07-15 10:16:51 +0800

    add get method

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 80
 
-app.use('/.well-known/acme-challenge', express.static('public'));
+try {
+    const acmeChallengeCode = fs.readFileSync('./acme-challenge-code.txt', 'utf-8')
+    const resourcePath = `/.well-known/acme-challenge/${acmeChallengeCode.split('.')[0]}`
+    app.get(resourcePath, (req, res) => {
+        res.send(acmeChallengeCode)
+    })
+    console.log(`create acme-challenge resource path: ${resourcePath}`)
+} catch (e) {
+    console.log(e)
+}
 
+app.use('/.well-known/acme-challenge', express.static('public'));
 app.listen(port, () => console.log(`app listening at port ${port}`))
 
 
